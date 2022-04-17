b8be7f63c0bee0ebffb8fa3e5344e3f61da31a2e
Scorpyo <saesscorpyo@gmail.com>
2022-04-17 16:51:07 +0100

    Formatting

--- client.lua
+++ client.lua
@@ -3,41 +3,41 @@ function loadSAESModelsLua()
     local file = fileOpen("modelsTable.lua")
     local content = fileRead(file, fileGetSize(file))
     fileClose(file)
-    
+
     loadstring(content)()
 end
 
-function startModels( )
-	for modelID, v in pairs( modelTable ) do
-		for i=1, #model_type do
-			local modelType = model_type[ i ]
-			local fileName = v[ modelType ]
-			if fileName then
-				local fileDirectory = fileName.."."..modelType;
-				if fileExists(fileDirectory) then
-					replaceModel( fileDirectory, modelID, modelType );
-				end
-			end
-		end
+function startModels()
+    for modelID, v in pairs(modelTable) do
+        for i = 1, #model_type do
+            local modelType = model_type[i]
+            local fileName = v[modelType]
+            if fileName then
+                local fileDirectory = fileName .. "." .. modelType;
+                if fileExists(fileDirectory) then
+                    replaceModel(fileDirectory, modelID, modelType);
+                end
+            end
+        end
 
-	end
+    end
 end
 
-function replaceModel( fileDirectory, modelID, modelType )
-	if modelType == "col" then
-		local col = engineLoadCOL( fileDirectory );
-		return engineReplaceCOL( col, modelID );
-	elseif modelType == "txd" then
-		local txd =	engineLoadTXD ( fileDirectory );
-		return engineImportTXD( txd, modelID );
-	elseif modelType == "dff" then
-		local dff = engineLoadDFF( fileDirectory, 0 );
-		return engineReplaceModel( dff, modelID, modelTable[modelID].alphaTransparency or false );
-	end
+function replaceModel(fileDirectory, modelID, modelType)
+    if modelType == "col" then
+        local col = engineLoadCOL(fileDirectory);
+        return engineReplaceCOL(col, modelID);
+    elseif modelType == "txd" then
+        local txd = engineLoadTXD(fileDirectory);
+        return engineImportTXD(txd, modelID);
+    elseif modelType == "dff" then
+        local dff = engineLoadDFF(fileDirectory, 0);
+        return engineReplaceModel(dff, modelID, modelTable[modelID].alphaTransparency or false);
+    end
 end
 
 function init()
     loadSAESModelsLua()
-    startModels( )
+    startModels()
 end
 init()
