40bbf75391ec27c314782129e1045c57748982d5
Scorpyo <saesscorpyo@gmail.com>
2022-04-17 16:50:59 +0100

    Fixing this repo after main resource got updated

--- client.lua
+++ client.lua
@@ -1,22 +1,43 @@
+local model_type = { "col", "txd", "dff" }
 function loadSAESModelsLua()
-    local file = fileOpen("models_client.lua")
+    local file = fileOpen("modelsTable.lua")
     local content = fileRead(file, fileGetSize(file))
     fileClose(file)
     
     loadstring(content)()
 end
 
-function overwriteDownloadExport()
-    exports.RPGfile = {
-        downloadFile = function(this, filepath)
-            return fileExists(filepath)
-        end,
-    }
+function startModels( )
+	for modelID, v in pairs( modelTable ) do
+		for i=1, #model_type do
+			local modelType = model_type[ i ]
+			local fileName = v[ modelType ]
+			if fileName then
+				local fileDirectory = fileName.."."..modelType;
+				if fileExists(fileDirectory) then
+					replaceModel( fileDirectory, modelID, modelType );
+				end
+			end
+		end
+
+	end
+end
+
+function replaceModel( fileDirectory, modelID, modelType )
+	if modelType == "col" then
+		local col = engineLoadCOL( fileDirectory );
+		return engineReplaceCOL( col, modelID );
+	elseif modelType == "txd" then
+		local txd =	engineLoadTXD ( fileDirectory );
+		return engineImportTXD( txd, modelID );
+	elseif modelType == "dff" then
+		local dff = engineLoadDFF( fileDirectory, 0 );
+		return engineReplaceModel( dff, modelID, modelTable[modelID].alphaTransparency or false );
+	end
 end
 
 function init()
     loadSAESModelsLua()
-    overwriteDownloadExport()
-    startModels()
+    startModels( )
 end
 init()
